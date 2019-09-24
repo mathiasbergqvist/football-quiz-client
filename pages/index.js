da7a8c868111bd5e3d5cbdce09f9124daebaa743
Mathias Bergqvist <mathiasbergqvist88@gmail.com>
2019-09-24 21:17:38 +0200

    Add basic routing

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,14 @@
+import Link from "next/link";
+
 const Index = () => (
     <div>
         <h1>Hello Football Quiz</h1>
+        <Link href="/new-game" title="New Game">
+            <button>New Game</button>
+        </Link>
+        <Link href="/high-score" title="High Score">
+            <button>High Score</button>
+        </Link>
     </div>
 );
 
