e0b9b8ba088e5837376cef74b65d5bcb80647e22
Mathias Bergqvist <mathiasbergqvist88@gmail.com>
2019-09-24 21:11:37 +0200

    Setup three basic pages

---
+++ pages/high-scrore.js
@@ -0,0 +1,8 @@
+
+export const HighScore = () => (
+    <div>
+      <h1>High Score</h1>
+    </div>
+);
+
+export default HighScore;
---
+++ pages/new-game.js
@@ -0,0 +1,8 @@
+
+export const NewGame = () => (
+    <div>
+      <h1>New game page</h1>
+    </div>
+);
+
+export default NewGame;
