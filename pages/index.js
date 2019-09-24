b07d69fe58b6fccbf1d4fe5bcf10ad792a2e43a7
Mathias Bergqvist <mathiasbergqvist88@gmail.com>
2019-09-24 21:51:06 +0200

    Add support for Google Font Lato and global styling

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,35 @@
 import Link from "next/link";
+import Head from "next/head";
+// import "./style.css";
 
 const Index = () => (
     <div>
-        <h1>Hello Football Quiz</h1>
+        <Head>
+            <title>Football Quiz</title>
+            <link
+                href="http://fonts.googleapis.com/css?family=Lato&subset=latin,latin-ext"
+                rel="stylesheet"
+                type="text/css"
+            ></link>
+        </Head>
+        <h1>Football Quiz</h1>
         <Link href="/new-game" title="New Game">
             <button>New Game</button>
         </Link>
         <Link href="/high-score" title="High Score">
             <button>High Score</button>
         </Link>
+        <style global jsx>{`
+            body {
+                margin: 0;
+                padding: 0;
+                font-family: "Lato", sans-serif;
+                font-size: 16px;
+                color: #333;
+                -webkit-font-smoothing: antialiased;
+                -moz-osx-font-smoothing: grayscale;
+            }
+        `}</style>
     </div>
 );
 
